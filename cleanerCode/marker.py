3a0be7e3a54831f6867d711e08feb5d5943d2314
Rob Johnson <Rob.Johnson@aggiemail.usu.edu>
2020-12-12 14:38:30 -0700

    Made a fix in the future, but had to reset it to the past.

--- cleanerCode/marker.py
+++ cleanerCode/marker.py
@@ -22,6 +22,7 @@ for file in files:
         if trigger and ':' in line:
             # We are now at the end of a function declaration. Add the bad line
             print(indent+badLine, file=temp)
+            trigger = False
 
     src.close()
     temp.close()
